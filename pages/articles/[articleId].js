a8e7b1f72e8c3841d3f2e272ad127fde6370c3df
Samuel Osinubi <samobafemisayo@gmail.com>
2023-02-27 12:06:55 +0100

    create Next App

---
+++ pages/articles/[articleId].js
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+
+function ArticlesDetailsPage () {
+const router = useRouter()
+
+
+
+  return <h1> The ArticlesDetailsPage</h1>
+}
+
+export default ArticlesDetailsPage
---
+++ pages/articles/index.js
@@ -0,0 +1,5 @@
+function ArticlesPage () {
+  return <h1> The Articles Page</h1>
+}
+
+export default ArticlesPage
