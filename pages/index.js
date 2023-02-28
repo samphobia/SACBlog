95a2b3d495dc02b842e62e0199407ad3b9e34103
Samuel Osinubi <samobafemisayo@gmail.com>
2023-02-28 14:25:59 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import MeetupList from "@/components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'My first blog Post',
+    image: 'https://unsplash.com/photos/OgqWLzWRSaI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
+    creator: 'Queen Ozone',
+    description: 'This is my firt post about my experience in sport development and talent aspirations'
+  },
+  {
+    id: 'm2',
+    title: 'My blog Post',
+    image: 'https://unsplash.com/photos/OgqWLzWRSaI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
+    creator: 'Queen Ozone',
+    description: 'This is my firt post about my experience in sport development and talent aspirations'
+  },
+  
+]
+
 function HomePage () {
-  return <h1> The Home Page</h1>
+  return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage
