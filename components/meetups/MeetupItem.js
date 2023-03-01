cfc47e89635715771c35aea2e7e1835ca04a4678
Samuel Osinubi <samobafemisayo@gmail.com>
2023-03-01 09:48:59 +0100

    articlesid page

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router'
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter()
+
+  function showDetailsHandler () {
+    router.push('/' + props.id)
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +20,7 @@ function MeetupItem(props) {
           <h4>{props.creator}</h4>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Read More</button>
         </div>
       </Card>
     </li>
---
+++ pages/[articlesId]/index.js
@@ -0,0 +1,16 @@
+// import { useRouter } from 'next/router';
+import { Fragment } from 'react'
+
+function ArticlesDetailsPage () {
+  return (
+    <Fragment>
+      <img src='https://unsplash.com/photos/OgqWLzWRSaI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'
+       alt='First Post' />
+       <h1>First Blog Post</h1>
+       <h3> Queen Ozone </h3>
+       <p> The Blogs description</p>
+    </Fragment> 
+  )
+}
+
+export default ArticlesDetailsPage
